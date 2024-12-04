80529fdb73dd341a89ec8d857c4131e4986b142e
RuslanWorkGit <lulka758@gmail.com>
2024-12-04 22:19:13 +0200

    create empty swiftUi project

---
+++ Homework5swiftUI/Homework5swiftUI/Homework5swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Homework5swiftUIApp.swift
+//  Homework5swiftUI
+//
+//  Created by Ruslan Liulka on 04.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Homework5swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
