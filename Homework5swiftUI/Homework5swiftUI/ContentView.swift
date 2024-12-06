8bb60bbfe46e43b86d04ac1c6fa8ff8c79f21f04
RuslanWorkGit <lulka758@gmail.com>
2024-12-06 12:28:43 +0200

    done task with SwiftUI

--- Homework5swiftUI/Homework5swiftUI/ContentView.swift
+++ Homework5swiftUI/Homework5swiftUI/ContentView.swift
@@ -7,16 +7,67 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    @State private var givenName: String = ""
+    @State private var familyName: String = ""
+    
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack(alignment: .leading) {
+                Text("Пошта:")
+                TextField(
+                    "Email",
+                    text: $givenName
+                )
+                .disableAutocorrection(true)
+                Text("Пароль:")
+                TextField(
+                    "Password",
+                    text: $familyName
+                )
+                .disableAutocorrection(true)
+            }
+            .textFieldStyle(.roundedBorder)
+            .padding([.leading, .trailing], horizontalSizeClass == .regular ? 80 : 16)
+            .padding(.bottom, 30)
+            
+            
+            VStack(spacing: 30) {
+                Button("Увійти") {
+                    
+                }
+                .frame(maxWidth: UIScreen.main.bounds.width * 0.4)
+                .foregroundColor(.white)
+                .padding(15)
+                .background(.blue)
+                .cornerRadius(10)
+                
+                
+                Button("Реєстрація") {
+                    
+                }
+                .frame(maxWidth: UIScreen.main.bounds.width * 0.4)
+                .foregroundColor(.white)
+                .padding(15)
+                .background(.blue)
+                .cornerRadius(10)
+                
+            }
+            
         }
-        .padding()
+        .offset(y: -50)
+        
+        
+        
     }
+        
+        
 }
 
 #Preview {
